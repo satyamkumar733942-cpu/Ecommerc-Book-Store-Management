73bb0544369158e832914c33dda2e67299d328e9
Satyam Kumar <your-email@example.com>
2026-03-31 07:58:55 +0530

    .

--- src/main/java/com/nareshit/config/WebConfig.java
+++ src/main/java/com/nareshit/config/WebConfig.java
@@ -7,13 +7,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-	// Source - https://stackoverflow.com/a/79191848
-	// Posted by Marwen Jaffel
-	// Retrieved 2026-03-31, License - CC BY-SA 4.0
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://protective-motivation-production-1c9a.up.railway.app")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
 
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**").allowedOrigins("https://protective-motivation-production-1c9a.up.railway.app")
-				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS").allowedHeaders("*").allowCredentials(true);
-	}
 }
