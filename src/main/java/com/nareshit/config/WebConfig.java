51add4850125bdd1ab9772b971a0e3ca803128d1
Satyam Kumar <your-email@example.com>
2026-03-31 08:20:41 +0530

    .

--- src/main/java/com/nareshit/config/WebConfig.java
+++ src/main/java/com/nareshit/config/WebConfig.java
@@ -7,13 +7,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("https://protective-motivation-production-1c9a.up.railway.app")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
-    }
+	 @Override
+	    public void addCorsMappings(CorsRegistry registry) {
+	        registry.addMapping("/**")
+	                .allowedOrigins(
+	                        "https://protective-motivation-production-1c9a.up.railway.app",
+	                        "http://localhost:8080"
+	                )
+	                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+	                .allowedHeaders("*")
+	                .allowCredentials(true);
 
 }
